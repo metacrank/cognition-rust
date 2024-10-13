3ba0bf6a949199c77449ac15c684454ead3755d3
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-10-12 19:09:58 -0700

    exported macro

--- src/macros.rs
+++ src/macros.rs
@@ -6,52 +6,57 @@ pub const COLOR_RESET: &[u8] = b"\x1B[0m";
 pub const DEFAULT_STACK_SIZE: usize = 24;
 pub const DEFAULT_STRING_LENGTH: usize = 24;
 
+#[macro_export]
 macro_rules! default_fprint_error {
   ($e:literal) => {
     print!("Value::fprint(): error: ");
     println!($e);
   }
 }
+#[macro_export]
 macro_rules! fwrite_check_byte {
   ($f:ident,$s:expr,$n:ident) => {
     let n = $n;
     match $f.write($s) {
       Ok(1) => {},
-      Ok(_) => { default_fprint_error!("not all bytes could be written");
-                 default_fprint_error!("wrote {n} bytes"); },
-      Err(e) => { default_fprint_error!("{e}"); },
+      Ok(_) => { $crate::default_fprint_error!("not all bytes could be written");
+                 $crate::default_fprint_error!("wrote {n} bytes"); },
+      Err(e) => { $crate::default_fprint_error!("{e}"); },
     }
   }
 }
+#[macro_export]
 macro_rules! fwrite_check {
   ($f:ident,$s:expr) => {
     let s: &[u8] = $s;
     for n in 0..s.len() {
-      fwrite_check_byte!($f, &s[n..n+1], n);
+      $crate::fwrite_check_byte!($f, &s[n..n+1], n);
     }
   }
 }
+#[macro_export]
 macro_rules! fwrite_check_pretty {
   ($f:ident,$s:expr) => {
     let s: &[u8] = $s;
     for n in 0..s.len() {
       match s[n] {
         b'\n' => {
-          fwrite_check_byte!($f, b"\\", n);
-          fwrite_check_byte!($f, b"n", n);
+          $crate::fwrite_check_byte!($f, b"\\", n);
+          $crate::fwrite_check_byte!($f, b"n", n);
         },
         b'\t' => {
-          fwrite_check_byte!($f, b"\\", n);
-          fwrite_check_byte!($f, b"t", n);
+          $crate::fwrite_check_byte!($f, b"\\", n);
+          $crate::fwrite_check_byte!($f, b"t", n);
         },
         _ => {
-          fwrite_check_byte!($f, &s[n..n+1], n);
+          $crate::fwrite_check_byte!($f, &s[n..n+1], n);
         },
       }
     }
   }
 }
 
+#[macro_export]
 macro_rules! add_func {
   ($wt:ident,$f:ident,$name:literal) => {
     //TODO: push $f into $wt with key derived from $name
