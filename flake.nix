c8eff74ce787239427bd17b88873fda2042948e3
Preston Pan <preston@nullring.xyz>
2024-10-20 01:55:04 -0700

    update flake.nix and format it

--- flake.nix
+++ flake.nix
@@ -5,36 +5,40 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
-      {
-        devShells.${system}.default = pkgs.mkShell
-          {
-            packages = with pkgs; [ rustc cargo binutils ];
-            shellHook = ''
-alias c="clear"
-alias l="ls -la"
-alias nr="nix run"
-alias nb="nix build"
-'';
-          };
-        defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-          name = "crank";
-          src = "${self}";
-          phases = [
-            "buildPhase"
-            "installPhase"
-          ];
-          buildPhase = with pkgs; ''
-        CARGO_TARGET_DIR=$out ${pkgs.cargo}/bin/cargo build --manifest-path $src/Cargo.toml --release --locked
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          binutils
+        ];
+        shellHook = ''
+          alias c="clear"
+          alias l="ls -la"
+          alias nr="nix run"
+          alias nb="nix build"
         '';
-          installPhase = with pkgs; ''
-mkdir -p $out/bin
-cp $out/release/cognition $out/bin/cognition
-'';
-        };
       };
+      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
+        name = "crank";
+        src = "${self}";
+        phases = [
+          "buildPhase"
+          "installPhase"
+        ];
+        buildPhase = with pkgs; ''
+          CARGO_TARGET_DIR=$out ${cargo}/bin/cargo build --manifest-path $src/Cargo.toml --release --locked
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $out/release/cognition $out/bin/cognition
+        '';
+      };
+    };
 }
